cfe1b6945fc80f8ed14b8f79c3aac7472b125dd0
Norrman95 <Filip.norrman@gmail.com>
2017-05-19 19:23:35 +0200

    Sprint & Stamina

--- Assets/PlayerStatus.cs
+++ Assets/PlayerStatus.cs
@@ -2,26 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStatus : MonoBehaviour {
-
+public class PlayerStatus : MonoBehaviour
+{
     public float health = 25;
+    public float currentStamina, maxStamina = 50;
+    public bool running;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (health <= 0)
         {
             Destroy(gameObject);
         }
-		
-	}
+        if (running)
+        {
+            currentStamina -= 1;
+        }
+        if (!running && currentStamina <= maxStamina)
+        {
+            currentStamina += 1;
+        }
+    }
     public void Damage(float damage)
     {
         health -= damage;
     }
-
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,35 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
-
+public class PlayerMovement : MonoBehaviour
+{
     private Rigidbody2D rigidB;
     public float acceleration;
+    public float maxSpeed;
     public float jumpForce;
     public LayerMask targetGround;
     Transform groundCheck;
     public bool isGrounded = false;
     public bool facingRight = true;
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         groundCheck = transform.FindChild("GroundCheck");
         rigidB = GetComponent<Rigidbody2D>();
 
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         CheckGrounded();
-        //Jump();
-        //Move();
-        //rigidB.velocity = Vector2.right * acceleration * Time.deltaTime;
-        
+
     }
     void FixedUpdate()
     {
         Jump();
+        Sprint();
         Move();
         rigidB.velocity = new Vector2(acceleration * Time.deltaTime, rigidB.velocity.y);
     }
@@ -39,40 +38,52 @@ public class PlayerMovement : MonoBehaviour {
         if (Input.GetKey(KeyCode.A))
         {
             facingRight = false;
-            if (acceleration > -150)
+            if (acceleration > -maxSpeed)
             {
                 acceleration -= 20;
             }
-
-
         }
         if (Input.GetKey(KeyCode.D))
         {
             facingRight = true;
-            if (acceleration < 150)
+            if (acceleration < maxSpeed)
             {
                 acceleration += 20;
-            }           
+            }
         }
         if (acceleration < 0 || acceleration > 0)
         {
             acceleration += Mathf.Sign(acceleration) * -4;
         }
-        
+
     }
 
     public void Jump()
     {
         if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
-            rigidB.AddForce(new Vector2(0,jumpForce));
+            rigidB.AddForce(new Vector2(0, jumpForce));
             isGrounded = false;
+        }
+    }
+
+    public void Sprint()
+    {
+        if (Input.GetButton("Sprint") && GetComponent<PlayerStatus>().currentStamina >= 1 && isGrounded)
+        {
+            GetComponent<PlayerStatus>().running = true;
+            maxSpeed = 350;
+        }
+        else
+        {
+            GetComponent<PlayerStatus>().running = false;
+            maxSpeed = 150;
         }
     }
 
     public void CheckGrounded()
     {
-        
+
         Vector2 groundCheckPos = new Vector2(groundCheck.position.x, groundCheck.position.y);
         RaycastHit2D chk = Physics2D.Raycast(groundCheckPos, Vector2.down, 0.17f, targetGround);
         if (chk.collider != null)
@@ -84,6 +95,6 @@ public class PlayerMovement : MonoBehaviour {
         {
             isGrounded = false;
         }
-        
+
     }
 }
